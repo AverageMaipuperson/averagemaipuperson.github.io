b0e8296b093a5c68ce36c727d566b29a3dcf5436
AverageMaipuperson <averagemaipuperson@gmail.com>
2026-06-22 20:52:36 -0400

    #3

---
+++ 2p2_to_omni/index.js
@@ -0,0 +1,13 @@
+const form = document.querySelector("form");
+const input = form.querySelector("input[type='file']");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const version = document.getElementById("version").value;
+
+    const reader = new FileReader();
+    reader.onload = function(e) {
+        const contents = e.target.result;
+        console.log(contents);
+    }
+});
