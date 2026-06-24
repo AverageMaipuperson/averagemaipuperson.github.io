69b33f7704aa72e42249eeca736fdc399a12e95f
AverageMaipuperson <averagemaipuperson@gmail.com>
2026-06-23 20:07:50 -0400

    #12

--- 2p2_to_omni/parser/parser.js
+++ 2p2_to_omni/parser/parser.js
@@ -1,5 +1,22 @@
-export function xml_parse(str)
-{
+export function xml_parse(str) {
     const parser = new DOMParser();
-    return parser.parseFromString(str, "text/xml");
+    const doc = parser.parseFromString(str, "text/xml");
+    const dict = doc.querySelector("dict");
+    const result = {};
+
+    if (!dict) return result;
+    const children = dict.children;
+    for (let i = 0; i < children.length; i += 2) {
+        const keyNode = children[i];
+        const valNode = children[i + 1];
+
+        if (keyNode && valNode && keyNode.tagName === 'k') {
+            const key = keyNode.textContent;
+            const val = valNode.tagName === 'i' ? Number(valNode.textContent) : valNode.textContent;
+            
+            result[key] = val;
+        }
+    }
+
+    return result;
 }
