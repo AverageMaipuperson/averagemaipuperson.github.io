8e3bfaa3e324278a2c090f5b7cfca72265c2c330
AverageMaipuperson <averagemaipuperson@gmail.com>
2026-06-24 00:11:00 -0400

    #34

--- 2p2_to_omni/parser/gd_string_parser.js
+++ 2p2_to_omni/parser/gd_string_parser.js
@@ -1,6 +1,8 @@
 export function get_objects(str)
 {
-    return str.split(";").shift();
+    let string = str.startsWith("kS38,") ? str.slice(5) : str;
+    const objs = string.split("|");
+    return objs.filter(obj => obj.includes("_") && obj.trim() !== "");
 }
 
 export function get_object_data(obj)
